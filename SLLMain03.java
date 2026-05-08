ad246ba7368447acaef8556b272c39ccc6f31e21
akmalfitrahari-sib <akmal.fitrahari@gmail.com>
2026-05-08 09:20:10 +0700

    Input keyboard

--- SLLMain03.java
+++ SLLMain03.java
@@ -1,18 +1,38 @@
+import java.util.Scanner;
 public class SLLMain03 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         SingleLinkedList03 sll = new SingleLinkedList03();
-        Mahasiswa03 mhs1 = new Mahasiswa03("24212200", "Alvaro", "1A", 4.0);
-        Mahasiswa03 mhs2 = new Mahasiswa03("23212201", "Bimon", "2B", 3.8);
-        Mahasiswa03 mhs3 = new Mahasiswa03("22212202", "Cintia", "3C", 3.5);
-        Mahasiswa03 mhs4 = new Mahasiswa03("21212203", "Dirga", "4D", 3.6);
-        
+
+        System.out.print("Masukkan jumlah mahasiswa: ");
+        int jumlah = sc.nextInt();
+        sc.nextLine();
+
+        for (int i = 0; i < jumlah; i++) {
+
+            System.out.println("\nData Mahasiswa ke-" + (i + 1));
+
+            System.out.print("NIM   : ");
+            String nim = sc.nextLine();
+
+            System.out.print("Nama  : ");
+            String nama = sc.nextLine();
+
+            System.out.print("Kelas : ");
+            String kelas = sc.nextLine();
+
+            System.out.print("IPK   : ");
+            double ipk = sc.nextDouble();
+            sc.nextLine();
+
+            Mahasiswa03 mhs = new Mahasiswa03(nama, nim, kelas, ipk);
+
+            sll.addLast(mhs);
+        }
+
+        System.out.println();
         sll.print();
-        sll.addFirst(mhs4);
-        sll.print();
-        sll.addLast(mhs1);
-        sll.print();
-        sll.insertAfter("Dirga", mhs3);
-        sll.insertAt(2, mhs2);
-        sll.print();
+
+        sc.close();
     }
 }
